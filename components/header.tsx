4a044ab4438cea9c6e4224e9235d769c946936a0
AishwaryaT19 <aishwarya.tewari19@gmail.com>
2023-01-30 23:55:43 +0530

    header added

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import { logo } from "./Info";
+import Link from "next/link";
 
-export default function header() {
-  return <div>header</div>;
+export default function Header() {
+  return (
+    <header id="header">
+      <Link className="img-container" href="/">
+        <Image src={logo} alt="back" fill sizes="100%" />
+      </Link>
+      <nav className="navigation">
+        <Link className="heading" href="/">
+          Home
+        </Link>
+        <Link className="heading" href="/about">
+          About
+        </Link>
+        <Link className="heading" href="/projects">
+          Projects
+        </Link>
+      </nav>
+    </header>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/styles.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Footer from "components/footer";
+import Header from "components/header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header />
       <Component {...pageProps} />;
       <Footer />
     </>
